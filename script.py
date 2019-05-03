be1c17de7aa2975c9c2f76df9edb7826bc0c3105
Davi William Moraes Suga <33470530+daviwms999@users.noreply.github.com>
2019-05-02 22:28:01 -0300

    Leitura de arquivo por linha de comando
    O script já é capaz de ler arquivo com formato específico. Nome do arquivo é passado por linha de comando.

--- script.py
+++ script.py
@@ -10,10 +10,28 @@ https://www.globo.com 1554390900 1554477300 *
 # <link da notícia> <categoria> <facebook tweeter> <ranking no portal origem>
 https://g1.globo.com/politica/noticia/2019/04/05/nao-existe-olavetes-contra-militares-diz-bolsonaro-sobre-disputa-entre-alas-do-governo.ghtml política 76 tweet
 '''
+import sys
+
+#Listas onde ficarão armazenadas os dados do arquivo de input.
+
+links=[]
+datasIniciais=[]
+datasFinais=[]
+qtdsNoticias=[]
+nomeDoArquivo = str(sys.argv[1])
 
 #Leitura do arquivo input.txt
 
+with open(nomeDoArquivo, "r") as arquivo:
+    for line in arquivo.readlines():
+        link, dataInicial, dataFinal, qtdNoticias = line.split(' ')
+        links.append(link)
+        datasIniciais.append(dataInicial)
+        datasFinais.append(dataFinal)
+        qtdsNoticias.append(qtdNoticias)
+    print(links, datasFinais, datasIniciais, qtdsNoticias)
+
+
+
 
 #Criação do arquivo output.txt (se não existir) e escrita no arquivo
-
-
