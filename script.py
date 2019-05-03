c80cf232604734e5bda7683d4600b7888efd26b3
renan490 <renan490@gmail.com>
2019-05-02 21:37:41 -0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+'''
+coletor input.txt output.txt
+
+# input.txt
+# <link portal de notícia> <data inicial timestamp> <data final timestamp> <qtd noticias>
+https://www.uol.com.br 1554390900 1554477300 10
+https://www.globo.com 1554390900 1554477300 *
+
+# output.txt
+# <link da notícia> <categoria> <facebook tweeter> <ranking no portal origem>
+https://g1.globo.com/politica/noticia/2019/04/05/nao-existe-olavetes-contra-militares-diz-bolsonaro-sobre-disputa-entre-alas-do-governo.ghtml política 76 tweet
+'''
